87ec8859f1b6c755e9f5f8bc2feeb2e5a47d931e
Christopher Castellano <c.castellano18@gmail.com>
2021-01-01 23:09:32 -0600

    spinner added

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,20 +1,33 @@
-import React, { Component } from 'react'
+import React from 'react'
 import UserItem from './UserItem'
+import Spinner from '../layout/Spinner'
+import PropTypes from 'prop-types'
 
-export class Users extends Component {
+//deconstructed props from the App.js
+const Users = ({users, loading}) => {
   //users are now props from the api call on App.js
-  render() {
+  
+  if(loading){
+    return <Spinner/>
+  }
+  else{
     return (
       <div style={userStyle}>
         
-        {this.props.users.map(user => (
+        {users.map(user => (
           //passing user as a prop to UserItem
           <UserItem key={user.id} user={user}/>
         ))}
+    
       </div>
     )
   }
-}
+  }
+
+  Users.propTypes ={
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired,
+  }
 
 const userStyle = {
   display: 'grid',
